2095a9d17e4c24f3145ee82df7fa86b99572f044
IlanVinograd <ilanv1111@gmail.com>
2026-02-07 19:48:16 +0200

    Implement arithmetic operators and stream output for Vec2 struct; enhance functionality and usability.

--- src/utilities/vec.hpp
+++ src/utilities/vec.hpp
@@ -1,14 +1,70 @@
 #ifndef VECTOR_H
 #define VECTOR_H
-
+//TODO: places which does operation change to new overloaded ones.
 namespace Optikos
 {
 struct Vec2
 {
+    Vec2(float x = 0.0f, float y = 0.0f) : x(x), y(y)
+    {
+    }
+
+    Vec2 operator+(const Vec2& other) const
+    {
+        return Vec2(x + other.x, y + other.y);
+    }
+
+    Vec2& operator+=(const Vec2& other)
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Vec2 operator-(const Vec2& other) const
+    {
+        return Vec2(x - other.x, y - other.y);
+    }
+
+    Vec2& operator-=(const Vec2& other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    Vec2 operator*(const Vec2& other) const
+    {
+        return Vec2(x * other.x, y * other.y);
+    }
+
+    Vec2& operator*=(const Vec2& other)
+    {
+        x *= other.x;
+        y *= other.y;
+        return *this;
+    }
+
+    Vec2 operator/(const Vec2& other) const
+    {
+        return Vec2(x / other.x, y / other.y);
+    }
+
+    Vec2& operator/=(const Vec2& other)
+    {
+        x /= other.x;
+        y /= other.y;
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Vec2& vec2)
+    {
+        os << vec2.x << " " << vec2.y;
+        return os;
+    }
+
     float x;
     float y;
-
-    // TODO: add operators
 };
 
 }  // namespace Optikos
